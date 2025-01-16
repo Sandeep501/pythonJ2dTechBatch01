4c61488ef463f89a60737de86d17e65cee3ba0e1
mete-rushi <meterushi021@gmail.com>
2025-01-16 09:14:15 +0530

    Lambdacode

---
+++ find_my_heroes.py
@@ -0,0 +1,15 @@
+
+
+heroes_name=input("enter your fan hero name")
+
+pan_india_heroes=[
+    'Prabhas',
+    'Mahesh Babu',
+    'Pavan kalyan',
+    'NTR',
+    'Allu Arjun'
+]
+
+if ((heroes_name.lower()) in pan_india_heroes)
+    or (heroes_name.upper()) in pan_india_heroes
+    or (heroes_name.):
---
+++ lamda.py
@@ -0,0 +1,9 @@
+
+#lambda function \ anonymous function | single line function
+
+my_num=int(input("enter a number :"))
+           
+is_even = lambda a: a%2==0
+
+print(is_even (my_num)  )
+
