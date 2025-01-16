e79e3ad58528ac5f7e3f9832f7a5d6c8e9f6d885
Vinod-Jogur <jogurvinod96@gmail.com>
2025-01-16 16:47:40 +0530

    Vinod's Branch

---
+++ HackerRank.py
@@ -0,0 +1,16 @@
+# ques 1
+print("Hello, World!")
+
+# ques 2
+n = int(input("enter the value for n:"))
+
+if n%2 ==1:
+    print ("Weird")
+elif n%2 ==0 and 2<=n<=5:
+    print ("Not Weird")
+elif n%2 ==0 and 6<=n<=20:
+    print ("Weird")
+else:
+    print ("Not Weird")
+
+# ques 3
